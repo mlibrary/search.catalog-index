cb2c71919275b8175fc256f02b9fd997d11caeed
Monique Rio <mrio@umich.edu>
2022-03-07 15:24:49 -0500

    added job for deleting; changed m_delete_ids to be executable

--- umich_catalog_indexing/bin/jobs.rb
+++ umich_catalog_indexing/bin/jobs.rb
@@ -6,3 +6,10 @@ class IndexIt
     system("bin/mindex_xml #{file}")
   end
 end
+class DeleteIt
+  include Sidekiq::Worker
+  def perform(file)
+    puts "indexing #{file}"
+    system("bin/m_delete_ids #{file}")
+  end
+end
