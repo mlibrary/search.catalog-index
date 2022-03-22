0c51d7c72a1687c07fcfdba14fe7eed75b00dd7a
Monique Rio <mrio@umich.edu>
2022-03-22 10:17:34 -0400

    updated mindex to have debug; fixed hathi

--- mock-ht/mock-ht.rb
+++ mock-ht/mock-ht.rb
@@ -4,6 +4,7 @@ use Rack::Auth::Basic, "Restricted Area" do |username, password|
   username == ENV.fetch("HT_USERNAME") and password == ENV.fetch("HT_PASSWORD")
 end
 
-get "/catalog/*" do
-  send_file "./file.json.gz"
+get "/catalog/zephir_upd_20220301.json.gz" do
+  logger.info("hi")
+  send_file "./zephir_upd_20220301.json.gz"
 end
