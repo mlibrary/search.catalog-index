74ebf1c0425fcfb477cb021fbe622bf32e28cdc0
Monique Rio <mrio@umich.edu>
2022-04-14 12:09:36 -0400

    added changes to get traject to work with 9.3

---
+++ umich_catalog_indexing/bin/m_get_high_level_browse.rb
@@ -0,0 +1,11 @@
+require "jruby"
+require "high_level_browse"
+path = "./lib/translation_maps"
+
+#does file exist and is file older than one day?
+if !File.exists?(path) or File.stat(path).mtime < Time.now - (60*60*24) 
+  HighLevelBrowse.fetch_and_save(dir: path)
+  puts "#{path}/hlb.json.gz is less than one day old. Did not update"
+else
+  puts "#{path}/hlb.json.gz is less than one day old. Did not update"
+end
