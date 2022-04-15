0649e714b58e94f74d85866ac33fda8fdae9af48
Monique Rio <mrio@umich.edu>
2022-04-15 16:12:25 -0400

    fixed high level browse checking for file existing

--- umich_catalog_indexing/bin/m_get_high_level_browse.rb
+++ umich_catalog_indexing/bin/m_get_high_level_browse.rb
@@ -3,7 +3,7 @@ require "high_level_browse"
 path = "./lib/translation_maps"
 
 #does file exist and is file older than one day?
-if !File.exists?(path) or File.stat(path).mtime < Time.now - (60*60*24) 
+if !File.exists?("#{path}/hlb.json.gz") or File.stat(path).mtime < Time.now - (60*60*24) 
   HighLevelBrowse.fetch_and_save(dir: path)
   puts "#{path}/hlb.json.gz is less than one day old. Did not update"
 else
