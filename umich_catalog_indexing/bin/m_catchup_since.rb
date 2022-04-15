08bb1466df65f697b5343a2252c7149972b975f9
Monique Rio <mrio@umich.edu>
2022-04-15 11:21:48 -0400

    fixed up option parses. Defaults to catching up today

--- umich_catalog_indexing/bin/m_catchup_since.rb
+++ umich_catalog_indexing/bin/m_catchup_since.rb
@@ -1,20 +1,27 @@
+#!/usr/local/bin/ruby
+
 require "optparse"
+require "optparse/date"
 require 'logger'
 require 'date'
 require_relative "../lib/index_for_date"
 require_relative "../lib/sftp"
 
 logger = Logger.new($stdout)
-date = '' 
+date = DateTime.now.strftime("%Y%m%d") 
 solr_url = ENV.fetch("REINDEX_SOLR_URL") 
 path = ENV.fetch("DAILY_ALMA_FILES_PATH")
 OptionParser.new do |opts|
-  opts.on("-d --date DATE") do |x|
+  opts.on("-d", "--date", Date, "Date from which to catchup from; Default is today") do |x|
     date = x 
   end
-  opts.on("-s", "--solr SOLR") do |x|
+  opts.on("-s", "--solr", "Solr url to index into. Default is #{solr_url}") do |x|
     solr_url = x
   end
+   opts.on("-h", "--help", "Prints this help") do
+     puts opts
+     exit
+   end
 end.parse!
 
 alma_files = SFTP.new.ls(path)
