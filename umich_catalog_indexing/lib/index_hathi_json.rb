df8acbe84f379f727dba0f9f8a91d7cdf5e35ac7
Monique Rio <mrio@umich.edu>
2022-04-27 16:24:41 -0400

    turned mindex_xml and mindex_hathi into ruby scripts

---
+++ umich_catalog_indexing/lib/index_hathi_json.rb
@@ -0,0 +1,44 @@
+require "logger"
+class IndexHathiJson
+  def initialize(file:, solr_url:, logger: Logger.new($stdout))
+    @file = file
+    @working_file = "/app/scratch/#{@file}" 
+    @logger = logger
+    @solr_url = solr_url
+  end
+  def run
+    @logger.info "fetching #{@file} from #{ENV.fetch("HT_HOST")}"
+    fetch_hathi
+    @logger.info "starting traject process for #{@working_file}"
+    run_traject
+    @logger.info "finished loading marc data from #{@working_file} into #{@solr_url}"
+    @logger.info "cleaning scratch directory"
+    clean
+    @logger.info "finished processing #{@file}"
+  end
+  def fetch_hathi
+    system("curl","-u",
+           "#{ENV.fetch("HT_USERNAME")}:#{ENV.fetch("HT_PASSWORD")}",
+           "#{ENV.fetch("HT_HOST")}/catalog/#{@file}", "-o",
+           @working_file
+          )
+  end
+  def run_traject
+    system( "bundle", "exec", "traject",
+           "-c", "/app/readers/ndj.rb",
+           "-c", "/app/writers/solr.rb",
+           "-c", "/app/indexers/settings.rb",
+           "-c", "/app/indexers/common.rb",
+           "-c", "/app/indexers/common_ht.rb",
+           "-c", "/app/indexers/subject_topic.rb",
+           "-c", "/app/indexers/umich.rb",
+           "-c", "/app/indexers/umich_alma.rb",
+           #"-s", "log.file=#{$stdout}",
+           "-u", @solr_url,
+           @working_file
+    )
+  end
+  def clean
+    File.delete(@working_file)
+  end
+end
---
+++ umich_catalog_indexing/spec/alma_file_processor_spec.rb
@@ -0,0 +1,43 @@
+require_relative './spec_helper.rb'
+require "alma_file_processor.rb"
+RSpec.describe AlmaFileProcessor do
+  before(:each) do
+    @src_path = "search_daily_bibs/file.tar.gz"
+  end
+  subject do
+    described_class.new(path: @src_path)
+  end
+  context "#run" do
+    before(:each) do
+      @tar_double = double("TarDouble", exec: "")
+      @run_params = {
+        sftp: instance_double(SFTP, get: ""),
+        tar: lambda{|path, destination| @tar_double.exec(path, destination)}
+      }
+    end
+    it "calls sftp get function with path and destination" do
+      expect(@run_params[:sftp]).to receive(:get).with(@src_path, "/app/scratch")
+      subject.run(**@run_params)
+    end
+    it "calls tar on the with the appropriate parms" do
+      expect(@tar_double).to receive(:exec).with("/app/scratch/file.tar.gz", "/app/scratch")
+      subject.run(**@run_params)
+    end
+  end 
+  context "#xml_file" do
+    it "returns the appropriate filename" do
+      expect(subject.xml_file).to eq("/app/scratch/file.xml")
+    end
+  end
+  context "#clean" do
+    before(:each) do
+      @file_delete_double = class_double(File, delete: nil)
+      @delete = lambda{|file| @file_delete_double.delete(file)}
+    end
+    it "removes the files put in the scratch directory" do
+      expect(@file_delete_double).to receive(:delete).with("/app/scratch/file.tar.gz")
+      expect(@file_delete_double).to receive(:delete).with("/app/scratch/file.xml")
+      subject.clean(@delete) 
+    end
+  end
+end
