d4682698c13a80ed2b53f76f250d0f2a14c3e720
Monique Rio <mrio@umich.edu>
2022-05-10 12:14:01 -0400

    fixed duplicate macc environment variable

--- umich_catalog_indexing/bin/index_latest_hathi_update.rb
+++ umich_catalog_indexing/bin/index_latest_hathi_update.rb
@@ -11,7 +11,7 @@ hathi_file = Jobs::Utilities::ZephirFile.latest_daily_update
 
 logger.info "Sending job to index #{hathi_file} into hatcher production solr: #{ENV.fetch("HATCHER_PRODUCTION_SOLR_URL")}"
 
-IndexHathi.perform_async(hathi_file, ENV.fetch("MACC_PRODUCTION_SOLR_URL"))
+IndexHathi.perform_async(hathi_file, ENV.fetch("HATCHER_PRODUCTION_SOLR_URL"))
 
 logger.info "Sending job to index #{hathi_file} into macc production solr: #{ENV.fetch("MACC_PRODUCTION_SOLR_URL")}"
 IndexHathi.perform_async(hathi_file, ENV.fetch("MACC_PRODUCTION_SOLR_URL"))
