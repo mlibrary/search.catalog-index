bdf8f5438cb08eeccf55d24606b1b8b6684fa794
Matthew Alexander LaChance <mattlach@umich.edu>
2022-06-07 18:04:17 -0400

    Use a pushgateway for index job metrics
    While sidekiq is itself an offline queue, we're interested in when
    IndexIt ran versus when IndexHathi ran, and those specific jobs are
    transient. When storing those metrics just in sidekiq, we had to deal
    with the inconsistent numbers coming from different replicas, and each
    replica would forget its past state on reboot.
    
    This is exactly the sort of thing that the pushgateway was designed for,
    but an offline queue (sidekiq) pushing metrics to a pushgateway was
    exactly not the sort of thing that Yabeda was designed for. Yabeda saves
    a lot of typing but assumes a field of metrics that will change over
    time (e.g. number of requests, number of books checked out).
    
    With cronjobs, pushing to a pushgateway works just fine, since the state
    will disappear with the cronjob. With sidekiq, the state was persisting
    between cronjobs, resulting in weird behavior, especially with multiple
    replicas trying to override each other.
    
    By using the prometheus client directly, we can init a new registry and
    push metrics with different grouping keys so that nothing overrides
    anything unexpected.

--- umich_catalog_indexing/lib/sidekiq_jobs.rb
+++ umich_catalog_indexing/lib/sidekiq_jobs.rb
@@ -1,38 +1,78 @@
 require "sidekiq"
-require "yabeda/sidekiq"
-require "yabeda/prometheus"
+require "prometheus/client"
+require "prometheus/client/push"
+require "prometheus/client/registry"
 require "/app/lib/jobs"
 
-Yabeda.configure do
-  gauge :indexing_job_last_success, comment: "Time the indexing last succeeded", tags: [:destination, :type]
-end
-Yabeda.configure!
+class CatalogIndexMetrics
+  def initialize(labels)
+    @labels = labels
+    @start_time = current_timestamp
+  end
 
-Sidekiq.configure_server do |_config|
-  Yabeda::Prometheus::Exporter.start_metrics_server!
+  def push
+    indexing_job_duration_seconds.set(current_timestamp - @start_time)
+    indexing_job_last_success.set(current_timestamp)
+    gateway.add(registry)
+  end
+
+  private
+
+  def current_timestamp
+    Time.now.to_i
+  end
+
+  def registry
+    @registry ||= Prometheus::Client::Registry.new
+  end
+
+  def gateway
+    @gateway ||= Prometheus::Client::Push.new(
+      job: "catalog_index",
+      gateway: ENV.fetch("PROMETHEUS_PUSH_GATEWAY"),
+      grouping_key: @labels
+    )
+  end
+
+  def indexing_job_last_success
+    @indexing_job_last_success ||= registry.gauge(
+      :indexing_job_last_success,
+      docstring: "Last successful run of an indexing job"
+    )
+  end
+
+  def indexing_job_duration_seconds
+    @indexing_job_duration_seconds = registry.gauge(
+      :indexing_job_duration_seconds,
+      docstring: "Time spent running an indexing job"
+    )
+  end
 end
 
 class IndexIt
   include Sidekiq::Worker
   def perform(file, solr_url)
     puts "indexing #{file} into #{solr_url}"
+    metrics = CatalogIndexMetrics.new({ type: "IndexIt", destination: solr_url })
     Jobs::IndexAlmaXml.new(file: file, solr_url: solr_url).run
-    Yabeda.indexing_job_last_success.set({type: "IndexIt", destination: solr_url},Time.now.to_i) 
+    metrics.push
   end
 end
 class DeleteIt
   include Sidekiq::Worker
   def perform(file, solr_url)
     puts "indexing deletes from #{file} into #{solr_url}"
+    metrics = CatalogIndexMetrics.new({ type: "DeleteIt", destination: solr_url })
     Jobs::DeleteAlmaIds.new(file: file, solr_url: solr_url).run
-    Yabeda.indexing_job_last_success.set({type: "DeleteIt", destination: solr_url},Time.now.to_i) 
+    metrics.push
   end
 end
 class IndexHathi
   include Sidekiq::Worker
   def perform(file, solr_url)
     puts "indexing zephir #{file} into #{solr_url}"
+    metrics = CatalogIndexMetrics.new({ type: "IndexHathi", destination: solr_url })
     Jobs::IndexHathiJson.new(file: file, solr_url: solr_url).run
-    Yabeda.indexing_job_last_success.set({type: "IndexHathi", destination: solr_url},Time.now.to_i) 
+    metrics.push
   end
 end
