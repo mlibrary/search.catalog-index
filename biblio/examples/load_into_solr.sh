3740e2cfe5d1cf9abda7db2e59debe7ecda29a39
Aaron Elkiss <aelkiss@umich.edu>
2022-06-17 17:21:31 -0400

    WIP: Build an image with records baked in
    - Run indexing during docker image build process
    
    - Faster indexing and progress reporting for records loaded
    
    - Inexplicably, /var/solr/biblio seems to disappear between layers; I
    can't for the life of me figure out why. Copying it aside and restoring
    it when starting seems to work but is very hacky.

--- biblio/examples/load_into_solr.sh
+++ biblio/examples/load_into_solr.sh
@@ -1,7 +1,22 @@
+#!/bin/bash
+
+SOLR_URL="http://localhost:8026/solr/biblio"
 input="/examples/sample1.json"
+echo "Indexing records into Solr..."
+nrec=$(cat $input | wc -l)
+i=0
 while IFS= read -r line
 do
-cat << EOT | curl -X POST -H 'Content-Type:application/json'  --data-binary @- http://localhost:8026/solr/biblio/update/json?commit=true
-[$line]
-EOT
+  let i++
+  cat <<-EOT | curl -s -X POST -H "Content-Type:application/json"  --data-binary @- "$SOLR_URL/update/json/docs" > /dev/null
+  [$line]
+	EOT
+  if (( $i % 50 == 0 || $i == $nrec )); then
+    echo -ne "$i / $nrec records indexed\r"
+  fi
 done < "$input"
+echo 
+
+echo "Committing"
+curl -s -H "Content-Type: application/json" -X POST -d'{"commit": {}}' "$SOLR_URL/update?wt=json"
+echo "Done"
