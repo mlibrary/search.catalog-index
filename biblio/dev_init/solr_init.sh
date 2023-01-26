530edba749fd1e6656a7188958c0b4e044b04668
Monique Rio <mrio@umich.edu>
2023-01-26 12:21:02 -0500

    updates Dockerfile; minor other changes

--- biblio/dev_init/solr_init.sh
+++ biblio/dev_init/solr_init.sh
@@ -4,7 +4,7 @@
 solr zk cp /var/solr/data/security.json zk:security.json -z zoo:2181
 
 # uploads the configuration in the biblio directory
-solr zk upconfig -z  zoo:2181 -n biblio -d /biblio
+#solr zk upconfig -z  zoo:2181 -n biblio -d /biblio
 
 # runs docker entry-point.sh and whatever is in command
 exec /opt/docker-solr/scripts/docker-entrypoint.sh "$@"
