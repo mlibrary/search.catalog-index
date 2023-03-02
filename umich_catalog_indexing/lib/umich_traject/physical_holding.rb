9a64e05d901b3ff6c420385c9dd4ea22eaf0d5c9
Bill Dueber <bill@dueber.com>
2023-03-02 11:43:52 -0500

    Clean up physical_holding selectors

--- umich_catalog_indexing/lib/umich_traject/physical_holding.rb
+++ umich_catalog_indexing/lib/umich_traject/physical_holding.rb
@@ -2,82 +2,70 @@ module Traject
   module UMich
     class PhysicalHolding
       attr_reader :holding_id
-        def initialize(record:, holding_id:)
-          @holding_id = holding_id 
-          @record = record
-        end
-        def summary_holdings
-          output = []
-          @record.each_by_tag("866") do |f|
-            output.push(f['a']) if f["8"] == holding_id
-          end
-          output.join(" : ")
-        end
 
-        def items
-          f974.map {|i| Traject::UMich::PhysicalItem.new(item: i, has_finding_aid: finding_aid?) }
-        end
+      def initialize(record:, holding_id:)
+        @holding_id = holding_id
+        @record = record
+      end
 
-        def callnumber
-          f852["h"]
+      def summary_holdings
+        output = []
+        @record.each_by_tag("866") do |f|
+          output.push(f['a']) if f["8"] == holding_id
         end
+        output.join(" : ")
+      end
 
-        def display_name
-          ::UMich::LibLocInfo.display_name(library, location)
-        end
+      def items
+        f974.map { |i| Traject::UMich::PhysicalItem.new(item: i, has_finding_aid: finding_aid?) }
+      end
 
-        def floor_location
-          return "" if callnumber.nil?
-          ::UMich::FloorLocation.resolve(library, location, callnumber)
-        end
+      def callnumber
+        f852["h"]
+      end
 
-        def info_link
-          ::UMich::LibLocInfo.info_link(library, location)
-        end
+      def display_name
+        ::UMich::LibLocInfo.display_name(library, location)
+      end
 
-        def library
-          f852["b"]
-        end
+      def floor_location
+        return "" if callnumber.nil?
+        ::UMich::FloorLocation.resolve(library, location, callnumber)
+      end
 
-        def location
-          f852["c"]
-        end
+      def info_link
+        ::UMich::LibLocInfo.info_link(library, location)
+      end
 
-        def public_note
-          f852["z"]
-        end
+      def library
+        f852["b"]
+      end
 
-        def finding_aid?
-          record_has_finding_aid = false
-          @record.each_by_tag("856") do |f|
-             link_text = f["y"] 
-             link = f["u"]
-             if link_text =~ /finding aid/i and link =~ /umich/i
-               record_has_finding_aid = true
-               break
-             end
-          end
-          record_has_finding_aid
-        end
+      def location
+        f852["c"]
+      end
 
-        def f852
-          @f852 ||= begin 
-            field = nil
-            @record.each_by_tag("852") do |f|
-              field = f if f["8"] == @holding_id 
-            end
-            field
-          end
-        end
-        def f974
-          @f974 ||= begin 
-            output = []
-            @record.each_by_tag("974") do |f|
-              output.push(f) if f["8"] == @holding_id 
-            end
-            output
-          end
-        end
+      def public_note
+        f852["z"]
+      end
+
+      def field_is_finding_aid?(f)
+        link_text = f["y"]
+        link = f["u"]
+        link_text =~ /finding aid/i and link =~ /umich/i
+      end
+
+      def finding_aid?
+        @record.fields("856").any? { |f| field_is_finding_aid?(f) }
+      end
+
+      def f852
+        @f852 ||= @record.fields("852").find { |f| f["8"] == @holding_id }
+      end
+
+      def f974
+        @f974 ||= @record.fields("974").select { |f| f["8"] == @holding_id }
+      end
     end
   end
 end
