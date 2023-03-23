9bf2bc1c9461a018aa357597a25a4b4cd9fb37b8
Monique Rio <mrio@umich.edu>
2023-03-23 16:43:34 -0400

    gets rid of the assess mechanism

--- umich_catalog_indexing/lib/jobs/index_alma_xml.rb
+++ umich_catalog_indexing/lib/jobs/index_alma_xml.rb
@@ -34,30 +34,19 @@ module Jobs
       @logger.info "finished processing #{@file}"
     end
     def run_traject(file)
-      if ENV.fetch("ASSESS")
-        success = system( "bundle", "exec", "traject",
-               "-c", "/app/readers/m4j.rb",
-               "-c", "/app/writers/null.rb",
-               "-c", "/app/indexers/settings.rb",
-               "-c", "/app/indexers/common.rb",
-               "-c", "/app/indexers/umich_alma.rb",
-               file
-        )
-      else
-        success = system( "bundle", "exec", "traject",
-               "-c", "/app/readers/m4j.rb",
-               "-c", "/app/writers/solr.rb",
-               "-c", "/app/indexers/settings.rb",
-               "-c", "/app/indexers/common.rb",
-               "-c", "/app/indexers/common_ht.rb",
-               "-c", "/app/indexers/subject_topic.rb",
-               "-c", "/app/indexers/umich.rb",
-               "-c", "/app/indexers/umich_alma.rb",
-               "-c", "/app/indexers/callnumbers.rb",
-               "-u", @solr_url,
-               file
-        )
-      end
+      success = system( "bundle", "exec", "traject",
+             "-c", "/app/readers/m4j.rb",
+             "-c", "/app/writers/solr.rb",
+             "-c", "/app/indexers/settings.rb",
+             "-c", "/app/indexers/common.rb",
+             "-c", "/app/indexers/common_ht.rb",
+             "-c", "/app/indexers/subject_topic.rb",
+             "-c", "/app/indexers/umich.rb",
+             "-c", "/app/indexers/umich_alma.rb",
+             "-c", "/app/indexers/callnumbers.rb",
+             "-u", @solr_url,
+             file
+      )
       raise StandardError unless success
     end
   end
