ca7fec34fe7fe27418f9962d5c06396277d58896
Monique Rio <mrio@umich.edu>
2023-04-05 15:13:57 -0400

    fix: turns off mail and storage

--- monitor/config/application.rb
+++ monitor/config/application.rb
@@ -1,6 +1,29 @@
 require_relative "boot"
 
-require "rails/all"
+#require "rails/all"
+require "rails"
+
+#this is documentation of what's been removed
+removed = %(
+  action_mailer/railtie
+  action_mailbox/engine
+  active_storage/engine
+  action_text/engine
+)
+
+%w(
+  active_record/railtie
+  action_controller/railtie
+  action_view/railtie
+  active_job/railtie
+  action_cable/engine
+  rails/test_unit/railtie
+).each do |railtie|
+  begin
+    require railtie
+  rescue LoadError
+  end
+end
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
