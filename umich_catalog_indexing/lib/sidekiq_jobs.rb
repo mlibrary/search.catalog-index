6073c0d47641628430cf22240df027ea66309b56
Monique Rio <mrio@umich.edu>
2023-04-25 16:15:20 -0400

    adds sidekiq supervisor

--- umich_catalog_indexing/lib/sidekiq_jobs.rb
+++ umich_catalog_indexing/lib/sidekiq_jobs.rb
@@ -10,8 +10,45 @@ Yabeda.configure do
 end
 Yabeda.configure!
 
-Sidekiq.configure_server do |_config|
+class JobQueued
+  def call(worker, job, queue, redis_pool)
+    response = Faraday.post("#{ENV.fetch("SIDEKIQ_SUPERVISOR_HOST")}/api/v1/jobs", {
+      job_id: job["jid"],
+      arguments: job["args"].to_json,
+      job_class: job["class"],
+      queue: queue
+    })
+    yield
+  end
+end
+
+Sidekiq.configure_client do |config|
+  config.client_middleware do |chain|
+    chain.add JobQueued
+  end
+end
+
+class CheckInCheckOut
+  def call(worker, job, queue)
+    response = Faraday.post("#{ENV.fetch("SIDEKIQ_SUPERVISOR_HOST")}/api/v1/jobs/#{job["jid"]}/started", {
+      arguments: job["args"].to_json,
+      job_class: job["class"],
+      queue: queue
+    })
+    yield
+    response = Faraday.post("#{ENV.fetch("SIDEKIQ_SUPERVISOR_HOST")}/api/v1/jobs/#{job["jid"]}/complete", {
+      arguments: job["args"].to_json,
+      job_class: job["class"],
+      queue: queue
+    })
+  end
+end
+
+Sidekiq.configure_server do |config|
   Yabeda::Prometheus::Exporter.start_metrics_server!
+  config.server_middleware do |chain|
+    chain.add CheckInCheckOut
+  end
 end
 
 SFTP.configure do |config|
