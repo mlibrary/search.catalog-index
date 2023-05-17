b2b6094e84cecac0b8c07cb18044af94cdff3820
Monique Rio <mrio@umich.edu>
2023-05-17 08:55:47 -0400

    adds beginning of support dbs

---
+++ support_dbs/bin/hathifiles_db_catchup.rb
@@ -0,0 +1,8 @@
+require_relative "../lib/hathifiles_database_actions"
+
+date_input = ARGV[0] || Date.today.to_s
+start_date = Date.parse(date_input)
+
+start_date.upto(Date.today) do |date|
+  HathifilesDatabaseActions::Updater.new(date: date.to_s).run
+end
