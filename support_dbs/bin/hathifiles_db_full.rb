b5ec158df97ce965e9843b2959f6fec126cf1a56
Monique Rio <mrio@umich.edu>
2023-05-17 12:46:09 -0400

    turns hathifiles actions in to sidekiq workers

--- support_dbs/bin/hathifiles_db_full.rb
+++ support_dbs/bin/hathifiles_db_full.rb
@@ -1,4 +1,4 @@
-require_relative "../lib/hathifiles_database_actions"
+require_relative "../sidekiq"
 
 date_input = ARGV[1] || Date.new(Date.today.year, Date.today.month, 1).to_s
-HathifilesDatabaseActions::Full.new(date: date_input).run
+HathifilesDatabaseFull.perform_async(date: date_input)
