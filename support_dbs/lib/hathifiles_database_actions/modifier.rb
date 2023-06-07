0f467426413dfc50d56272ecd7089244f551fb03
Monique Rio <mrio@umich.edu>
2023-06-07 09:38:11 -0400

    didn't have an if statement for erroring on download

--- support_dbs/lib/hathifiles_database_actions/modifier.rb
+++ support_dbs/lib/hathifiles_database_actions/modifier.rb
@@ -22,7 +22,7 @@ module HathifilesDatabaseActions
         builder.adapter Faraday.default_adapter
       end 
       response = http_conn.get @hathifile_url
-      raise StandardError, "Did not successfully download #{@hathifile_url}"
+      raise StandardError, "Did not successfully download #{@hathifile_url}" if response.status != 200
       File.open("#{@scratch_dir}/#{hathifile}", 'wb') { |f| f.write(response.body) }
    
       command
