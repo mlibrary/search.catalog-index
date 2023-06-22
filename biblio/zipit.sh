fcc66ac4319967626b388c4e0bf6e9fbe6fef0dc
Monique Rio <mrio@umich.edu>
2023-06-22 13:09:40 -0400

    ci: adds start reindex action

--- biblio/zipit.sh
+++ biblio/zipit.sh
@@ -1,8 +1,14 @@
+#user=admin
+#password='b(@2]UDcua#Y8XpI'
+#host='https://index-search-solrcloud.search.lib.umich.edu'
+user=solr
+password=SolrRocks
+host=http://localhost:8983
 cd conf
 rm ../biblio.zip
 zip -r ../biblio.zip .
 cd ..
-curl -u solr:SolrRocks -X DELETE   "http://localhost:8983/api/cluster/configs/biblio"
-curl -u solr:SolrRocks -X PUT   --header "Content-Type: application/octet-stream"   --data-binary @biblio.zip   "http://localhost:8983/api/cluster/configs/biblio"
-curl -u solr:SolrRocks 'http://localhost:8983/solr/admin/collections?action=DELETE&name=biblio'
-curl -u solr:SolrRocks 'http://localhost:8983/solr/admin/collections?action=CREATE&name=biblio&numShards=1&collection.configName=biblio'
+curl -u $user:$password -X DELETE   "$host/api/cluster/configs/biblio"
+curl -u $user:$password -X PUT   --header "Content-Type: application/octet-stream"   --data-binary @biblio.zip   "$host/api/cluster/configs/biblio"
+curl -u $user:$password "$host/solr/admin/collections?action=DELETE&name=biblio"
+curl -u $user:$password "$host/solr/admin/collections?action=CREATE&name=biblio&numShards=1&collection.configName=biblio"
