577d69e1627fe0faeee9d1f443353f1c462d33fb
Monique Rio <mrio@umich.edu>
2024-02-26 13:28:30 -0500

    linting translation_map_fetcher.rb

--- umich_catalog_indexing/lib/jobs/utilities/translation_map_fetcher.rb
+++ umich_catalog_indexing/lib/jobs/utilities/translation_map_fetcher.rb
@@ -2,9 +2,10 @@ require "securerandom"
 module Jobs
   module Utilities
     class TranslationMapFetcher
-      def initialize(logger=Logger.new($stdout))
+      def initialize(logger = Logger.new($stdout))
         @logger = logger
       end
+
       def run
         @logger.info "fetching high level browse file"
         fetch_high_level_browse
@@ -13,53 +14,33 @@ module Jobs
         @logger.info "fetching electronic collections info"
         fetch_electronic_collections
       end
+
       private
+
       def fetch_high_level_browse
-        if should_fetch?(hlb_file) 
+        if should_fetch?(hlb_file)
           HighLevelBrowse.fetch_and_save(dir: hlb_dir)
           @logger.info "updated #{hlb_file}"
         else
           @logger.info "#{hlb_file} is less than one day old. Did not update"
         end
       end
+
       def fetch_lib_loc_info
-        fetch_translation_map(path: lib_loc_info_file, fetcher: lambda{ Jobs::LibLocInfo::LibraryLocationList.new.list.to_yaml(line_width: 1000 ) })
-        #if should_fetch?(lib_loc_info_file)
-          #temporary_path = "#{lib_loc_info_file}_#{SecureRandom.alphanumeric(8)}.temporary"
-          #File.open(temporary_path, 'w'){|f| f.write Jobs::LibLocInfo::LibraryLocationList.new.list.to_yaml(line_width: 1000 ) }
-          #if !File.exists?(temporary_path) || File.size?(temporary_path) < 15
-            #@logger.error "Did not update #{lib_loc_info_file}. Failed to load file"
-          #else
-            #File.rename(temporary_path, lib_loc_info_file)
-            #@logger.info "updated #{lib_loc_info_file}"
-          #end
-        #else
-          #@logger.info "#{lib_loc_info_file} is less than one day old. Did not update"
-        #end
+        fetch_translation_map(path: lib_loc_info_file, fetcher: lambda { Jobs::LibLocInfo::LibraryLocationList.new.list.to_yaml(line_width: 1000) })
       end
+
       def fetch_electronic_collections
-        fetch_translation_map(path: electronic_collection_file, fetcher: lambda{ Jobs::ElectronicCollections.fetch.to_yaml(line_width: 1000 ) })
-        #if should_fetch?(electronic_collection_file)
-          #temporary_path = "#{electronic_collection_file}_#{SecureRandom.alphanumeric(8)}.temporary"
-          #File.open(temporary_path, 'w'){|f| f.write Jobs::ElectronicCollections.fetch.to_yaml(line_width: 1000 ) }
-          #if !File.exists?(temporary_path) || File.size?(temporary_path) < 15
-            #@logger.error "Did not update #{lib_loc_info_file}. Failed to load file"
-          #else
-            #File.rename(temporary_path, lib_loc_info_file)
-            #@logger.info "updated #{lib_loc_info_file}"
-          #end
-        #else
-          #@logger.info "#{lib_loc_info_file} is less than one day old. Did not update"
-        #end
+        fetch_translation_map(path: electronic_collection_file, fetcher: lambda { Jobs::ElectronicCollections.fetch.to_yaml(line_width: 1000) })
       end
-      # [TODO:description]
+
       # @param path [String] [path to where the translation map should be saved]
       # @param fetcher [[Proc]] [The block of code that generates the string to
       # be written to a file]
-      def fetch_translation_map(path:,fetcher:)
+      def fetch_translation_map(path:, fetcher:)
         if should_fetch?(path)
           temporary_path = "#{path}_#{SecureRandom.alphanumeric(8)}.temporary"
-          File.open(temporary_path, 'w'){|f| f.write fetcher.call }
+          File.write(temporary_path, fetcher.call)
           if !File.exist?(temporary_path) || File.size?(temporary_path) < 15
             @logger.error "Did not update #{path}. Failed to load file"
           else
@@ -69,22 +50,26 @@ module Jobs
         else
           @logger.info "#{path} is less than one day old. Did not update"
         end
+      end
 
-      end
       def should_fetch?(file)
-        #true when file doesn't exit or if file is older than a day
-        !File.exist?(file) or 
-          File.stat(file).mtime < Time.now - (60*60*24) 
+        # true when file doesn't exit or if file is older than a day
+        !File.exist?(file) or
+          File.stat(file).mtime < Time.now - (60 * 60 * 24)
       end
+
       def hlb_dir
         "/app/lib/translation_maps"
       end
+
       def hlb_file
         "#{hlb_dir}/hlb.json.gz"
       end
+
       def lib_loc_info_file
         "/app/lib/translation_maps/umich/libLocInfo.yaml"
       end
+
       def electronic_collection_file
         "/app/lib/translation_maps/umich/electronic_collections.yaml"
       end
