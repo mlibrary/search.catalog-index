c9829966a363de984978c7a6f06a48bca57c7dff
Monique Rio <mrio@umich.edu>
2024-06-03 15:40:41 -0400

    removed indexing HT from index_alma_for_date

--- umich_catalog_indexing/lib/index_alma_for_date.rb
+++ umich_catalog_indexing/lib/index_alma_for_date.rb
@@ -1,55 +1,48 @@
 $:.unshift "#{File.dirname(__FILE__)}"
-require 'logger'
-require 'date'
+require "logger"
+require "date"
 require "sidekiq_jobs"
 require "jobs"
 
-class IndexForDate
-  def initialize(alma_files:,date:,solr_url:,
-                 delete_it: DeleteIt.new,
-                 index_it: IndexIt.new,
-                 index_hathi: IndexHathi.new,
-                 logger: Logger.new($stdout)
-                )
-    @date = DateTime.parse(date) #must be a string in the form YYYYMMDD
+class IndexAlmaForDate
+  def initialize(alma_files:, date:, solr_url:,
+    delete_it: DeleteIt.new,
+    index_it: IndexIt.new)
+    @date = DateTime.parse(date) # must be a string in the form YYYYMMDD
 
     begin
-      @alma_files = alma_files.select{|x| x.match?(date_string(@date))} #must be an array of file paths
+      @alma_files = alma_files.select { |x| x.match?(date_string(@date)) } # must be an array of file paths
     rescue NoMethodError
       raise StandardError, "alma_files must be an array of file path strings"
     end
 
     @solr_url = solr_url
     raise StandardError, "solr_url must be a string of the solr to index into" unless @solr_url.is_a? String
-    
+
     @delete_it = delete_it
     @index_it = index_it
-    @index_hathi = index_hathi
-    @logger = logger
+    @logger = S.logger
   end
 
   def run
     @logger.info("Indexing Alma Medata")
-    files_that_match(/_delete_?\d?\d?\.tar/).each do | file |
+    files_that_match(/_delete_?\d?\d?\.tar/).each do |file|
       @logger.info("deleting ids from file: #{file}")
       @delete_it.perform(file, @solr_url)
     end
-    files_that_match(/_new_?\d?\d?\.tar/).each do | file |
+    files_that_match(/_new_?\d?\d?\.tar/).each do |file|
       @logger.info("Indexing metadata from file: #{file}")
       @index_it.perform(file, @solr_url)
     end
+  end
 
-    @logger.info("Indexing HathiTrust metadata from file: #{hathi_file}")
-    @index_hathi.perform(hathi_file, @solr_url)
-  end
   private
+
   def files_that_match(pattern)
-    @alma_files.select{|x| x.match?(pattern)}
+    @alma_files.select { |x| x.match?(pattern) }
   end
-  def hathi_file
-    Jobs::Utilities::ZephirFile.daily_update(@date)
-  end
+
   def date_string(date)
-    date.strftime("%Y%m%d") #must be a string in the form YYYYMMDD
+    date.strftime("%Y%m%d") # must be a string in the form YYYYMMDD
   end
 end
--- umich_catalog_indexing/spec/index_alma_for_date_spec.rb
+++ umich_catalog_indexing/spec/index_alma_for_date_spec.rb
@@ -1,7 +1,7 @@
-require_relative './spec_helper.rb'
-require 'index_for_date.rb'
-RSpec.describe IndexForDate  do 
-  before(:each) do 
+require_relative "spec_helper"
+require "index_alma_for_date"
+RSpec.describe IndexAlmaForDate do
+  before(:each) do
     @params = {
       date: "20220101",
       alma_files: [
@@ -13,13 +13,11 @@ RSpec.describe IndexForDate  do
         "/some_directory/file3_20220101_new_15.tar.gz",
         "/some_directory/file1_different_date_20220102_new.tar.gz",
         "/some_directory/file1_different_date_20220102_delete.tar.gz",
-        "/some_directory/new_delete_file2_invalid.tar.gz",
+        "/some_directory/new_delete_file2_invalid.tar.gz"
       ],
       solr_url: "http://solr",
       index_it: instance_double(IndexIt, perform: nil),
-      delete_it: instance_double(DeleteIt, perform: nil),
-      index_hathi: instance_double(IndexHathi, perform: nil),
-      logger: instance_double(Logger, info: nil)
+      delete_it: instance_double(DeleteIt, perform: nil)
     }
   end
   subject do
@@ -28,15 +26,15 @@ RSpec.describe IndexForDate  do
   context "initialize" do
     it "raises an error when date is in the wrong format" do
       @params[:date] = "not a date string"
-      expect{ subject }.to raise_error(ArgumentError, "invalid date")
+      expect { subject }.to raise_error(ArgumentError, "invalid date")
     end
     it "raises an error when alma files are not an array of strings" do
       @params[:alma_files] = "not an array of strings"
-      expect{ subject }.to raise_error(StandardError, "alma_files must be an array of file path strings")
+      expect { subject }.to raise_error(StandardError, "alma_files must be an array of file path strings")
     end
     it "raises an error if solr_url is not a string" do
       @params[:solr_url] = []
-      expect{ subject }.to raise_error(StandardError, "solr_url must be a string of the solr to index into")
+      expect { subject }.to raise_error(StandardError, "solr_url must be a string of the solr to index into")
     end
   end
   context "#run" do
@@ -50,10 +48,6 @@ RSpec.describe IndexForDate  do
       expect(@params[:delete_it]).to receive(:perform).with("/some_directory/file1_20220101_delete.tar.gz", @params[:solr_url])
       expect(@params[:delete_it]).to receive(:perform).with("/some_directory/file2_20220101_delete_1.tar.gz", @params[:solr_url])
       expect(@params[:delete_it]).to receive(:perform).with("/some_directory/file3_20220101_delete_15.tar.gz", @params[:solr_url])
-      subject.run
-    end
-    it "calls IndexHathi with appropriate files and solr" do
-      expect(@params[:index_hathi]).to receive(:perform).with("zephir_upd_20211231.json.gz", @params[:solr_url])
       subject.run
     end
     it "does not call IndexIt when no new files" do
