847b2c0b739c00be1f9e3208c7f5a4e0f795e129
Monique Rio <mrio@umich.edu>
2024-07-08 12:47:03 -0400

    generate newer form of subject heading mapping

--- umich_catalog_indexing/lib/jobs/translation_map_generator/subject_heading_remediation.rb
+++ umich_catalog_indexing/lib/jobs/translation_map_generator/subject_heading_remediation.rb
@@ -1,6 +1,97 @@
 module Jobs
   module TranslationMapGenerator
     module SubjectHeadingRemediation
+      include FileWriter
+      def name
+        "Subject Headings mapping"
+      end
+
+      # @returns [String] where in the translation map directory the file
+      # should go
+      def file_path
+        File.join("umich", "subject_heading_remediation.yaml")
+      end
+
+      class Set
+        def self.for(id)
+          resp = AlmaRestClient.client.get_all(url: "conf/sets/#{id}/members", record_key: "members")
+          raise StandardError, "Couldn't retrieve authority set data for #{id}" if resp.status != 200
+          new(resp.body)
+        end
+
+        def initialize(data)
+          @data = data
+        end
+
+        def ids
+          @data["member"].map { |x| x["id"] }
+        end
+
+        def authority_records
+          ids.map do |id|
+            Authority.for(id)
+          end
+        end
+
+        def to_a
+          authority_records.map { |x| x.to_h }
+        end
+      end
+
+      class Authority
+        SUBFIELDS = ["a", "x", "v", "y", "z"]
+        def self.for(authority_record_id)
+          resp = AlmaRestClient.client.get("bibs/authorities/#{authority_record_id}", query: {view: "full"})
+          raise StandardError, "Couldn't retrieve authority data for #{authority_record_id}" if resp.status != 200
+          new(resp.body)
+        end
+
+        def initialize(data)
+          @record = MARC::XMLReader.new(StringIO.new(data["anies"]&.first)).first
+        end
+
+        def remediated_term
+          out_hash = Hash.new { |h, key| h[key] = [] }
+          @record.fields("150").first.subfields.each do |sf|
+            out_hash[sf.code].push(sf.value) if SUBFIELDS.include?(sf.code)
+          end
+          out_hash
+        end
+
+        def deprecated_terms
+          @record.fields("450").map do |field|
+            out_hash = Hash.new { |h, key| h[key] = [] }
+            field.subfields.each do |sf|
+              out_hash[sf.code].push(sf.value) if SUBFIELDS.include?(sf.code)
+            end
+            out_hash
+          end.sort do |a, b|
+            b.keys.count <=> a.keys.count
+          end
+        end
+
+        def to_h
+          {
+            "150" => remediated_term,
+            "450" => deprecated_terms
+          }
+        end
+
+        def _a_and_x(field)
+          @record.fields(field).map do |field|
+            x = field.subfields.filter_map { |x| x.value if x.code == "x" }
+            [field["a"], x].flatten.compact.join("--")
+          end.uniq
+        end
+
+        def to_a
+          deprecated_terms.map do |term|
+            [term.downcase, remediated_term]
+          end.sort do |a, b|
+            b.first <=> a.first
+          end.to_h
+        end
+      end
     end
   end
 end
--- umich_catalog_indexing/spec/jobs/translation_map_generator/subject_heading_remediation_spec.rb
+++ umich_catalog_indexing/spec/jobs/translation_map_generator/subject_heading_remediation_spec.rb
@@ -1,26 +1,38 @@
 require_relative "../../spec_helper"
 require "jobs"
 
-describe Jobs::TranslationMapGenerator::SubjectHeadingRemediation::ToDeprecated do
-  before(:each) do
-    @data = {
-      "illegal aliens" => "Undocumented immigrants",
-      "aliens" => "Undocumented immigrants",
-      "aliens, illegal" => "Undocumented immigrants",
-      "illegal immigrants" => "Undocumented immigrants",
-      "children of illegal aliens" => "Children of undocumented immigrants"
-    }
-  end
-  context ".reverse_it" do
-    it "reverses the input, using pipes as delimeters" do
-      expect(described_class.reverse_it(@data)).to eq({
-        "undocumented immigrants" => "illegal aliens||aliens||aliens, illegal||illegal immigrants",
-        "children of undocumented immigrants" => "children of illegal aliens"
-      })
-    end
-  end
+def remediated_term
+  {"a" => ["Undocumented immigrants"]}
 end
-describe Jobs::TranslationMapGenerator::SubjectHeadingRemediation::ToRemediated::Set do
+
+def deprecated_terms
+  [
+    {
+      "a" => ["Aliens"],
+      "x" => ["Legal status, laws, etc."]
+    },
+    {
+      "a" => ["Illegal aliens"],
+      "x" => ["Legal status, laws, etc."]
+    },
+    {
+      "a" => ["Undocumented foreign nationals"]
+    },
+    {
+      "a" => ["Illegal aliens"]
+    },
+    {
+      "a" => ["Aliens, Illegal"]
+    },
+    {
+      "a" => ["Illegal immigrants"]
+    },
+    {
+      "a" => ["Undocumented noncitizens"]
+    }
+  ]
+end
+describe Jobs::TranslationMapGenerator::SubjectHeadingRemediation::Set do
   before(:each) do
     @data = fixture("authority_set.json")
   end
@@ -62,12 +74,12 @@ describe Jobs::TranslationMapGenerator::SubjectHeadingRemediation::ToRemediated:
   context "#authority_records" do
     it "returns an array of Authority objects" do
       stub_authority_request
-      expect(subject.authority_records.first.remediated_term).to eq("Undocumented immigrants")
+      expect(subject.authority_records.first.remediated_term).to eq({"a" => ["Undocumented immigrants"]})
     end
   end
 
-  context "#to_h" do
-    it "returns a flattend hash of the array of authority objects" do
+  context "#to_a" do
+    it "returns an array of authority objects" do
       # Add an extra member to json
       d = JSON.parse(@data)
       d["member"].push({"id" => "999", "description" => "string"})
@@ -75,15 +87,42 @@ describe Jobs::TranslationMapGenerator::SubjectHeadingRemediation::ToRemediated:
       stub_authority_request
       stub_second_authority_request
 
-      expect(subject.to_h).to eq({
-        "undocumented foreign nationals" => "Undocumented immigrants",
-        "illegal aliens" => "Undocumented immigrants",
-        "aliens" => "Undocumented immigrants",
-        "aliens, illegal" => "Undocumented immigrants",
-        "illegal immigrants" => "Undocumented immigrants",
-        "undocumented noncitizens" => "Undocumented immigrants",
-        "stuff" => "Whatever"
-      })
+      expect(subject.to_a).to eq(
+        [
+          {
+            "150" => remediated_term,
+            "450" => deprecated_terms
+          },
+          {
+            "150" => {
+              "a" => ["Whatever"],
+              "x" => ["First x field", "Second x field"],
+              "v" => ["First v field", "Second v field"],
+              "y" => ["First y field", "Second y field"],
+              "z" => ["First z field", "Second z field"]
+            },
+            "450" => [
+              {
+                "a" => ["Stuff"],
+                "x" => ["First deprecated x field", "Second deprecated x field"],
+                "v" => ["First deprecated v field", "Second deprecated v field"],
+                "y" => ["First deprecated y field", "Second deprecated y field"],
+                "z" => ["First deprecated z field", "Second deprecated z field"]
+              }
+            ]
+          }
+        ]
+        # {
+        # "undocumented foreign nationals" => "Undocumented immigrants",
+        # "illegal aliens" => "Undocumented immigrants",
+        # "illegal aliens--legal status, laws, etc." => "Undocumented immigrants",
+        # "aliens--legal status, laws, etc." => "Undocumented immigrants",
+        # "aliens, illegal" => "Undocumented immigrants",
+        # "illegal immigrants" => "Undocumented immigrants",
+        # "undocumented noncitizens" => "Undocumented immigrants",
+        # "stuff--things--trivia" => "Whatever--Doesn't Matter--Another one"
+        # }
+      )
     end
   end
   context ".for" do
@@ -101,7 +140,7 @@ describe Jobs::TranslationMapGenerator::SubjectHeadingRemediation::ToRemediated:
     end
   end
 end
-describe Jobs::TranslationMapGenerator::SubjectHeadingRemediation::ToRemediated::Authority do
+describe Jobs::TranslationMapGenerator::SubjectHeadingRemediation::Authority do
   before(:each) do
     @data = JSON.parse(fixture("remediated_authority_record.json"))
   end
@@ -119,33 +158,184 @@ describe Jobs::TranslationMapGenerator::SubjectHeadingRemediation::ToRemediated:
       expect { described_class.for(authority_record_id) }.to raise_error(StandardError, /#{authority_record_id}/)
     end
   end
-  context "#remeidated_term" do
+  context "#remediated_term" do
     it "returns the remediated term" do
-      expect(subject.remediated_term).to eq("Undocumented immigrants")
+      expect(subject.remediated_term).to eq(remediated_term)
     end
   end
   context "#deprecated_terms" do
     it "returns the deprecated terms from the 450 field" do
-      expect(subject.deprecated_terms).to contain_exactly(
-        "Undocumented foreign nationals",
-        "Illegal aliens",
-        "Aliens",
-        "Aliens, Illegal",
-        "Illegal immigrants",
-        "Undocumented noncitizens"
-      )
+      expect(subject.deprecated_terms).to contain_exactly(*deprecated_terms)
     end
   end
   context "#to_h" do
     it "returns the expected deprecated_to_remediated hash with downcased terms" do
       expect(subject.to_h).to eq({
-        "undocumented foreign nationals" => "Undocumented immigrants",
-        "illegal aliens" => "Undocumented immigrants",
-        "aliens" => "Undocumented immigrants",
-        "aliens, illegal" => "Undocumented immigrants",
-        "illegal immigrants" => "Undocumented immigrants",
-        "undocumented noncitizens" => "Undocumented immigrants"
+        "150" => remediated_term,
+        "450" => deprecated_terms
       })
     end
   end
 end
+# describe Jobs::TranslationMapGenerator::SubjectHeadingRemediation::ToDeprecated do
+# before(:each) do
+# @data = {
+# "illegal aliens" => "Undocumented immigrants",
+# "aliens" => "Undocumented immigrants",
+# "aliens, illegal" => "Undocumented immigrants",
+# "illegal immigrants" => "Undocumented immigrants",
+# "children of illegal aliens" => "Children of undocumented immigrants"
+# }
+# end
+# context ".reverse_it" do
+# it "reverses the input, using pipes as delimeters" do
+# expect(described_class.reverse_it(@data)).to eq({
+# "undocumented immigrants" => "illegal aliens||aliens||aliens, illegal||illegal immigrants",
+# "children of undocumented immigrants" => "children of illegal aliens"
+# })
+# end
+# end
+# end
+# describe Jobs::TranslationMapGenerator::SubjectHeadingRemediation::ToRemediated::Set do
+# before(:each) do
+# @data = fixture("authority_set.json")
+# end
+# let(:set_id) { "1234" }
+# let(:authority_record_id) { "98187481368106381" }
+# let(:authority_record) { fixture("remediated_authority_record.json") }
+# let(:second_authority_record_id) { "999" }
+# let(:second_authority_record) { fixture("remediated_authority_record2.json") }
+# let(:stub_set_request) {
+# stub_alma_get_request(
+# url: "conf/sets/#{set_id}/members",
+# query: {limit: 100, offset: 0},
+# output: @data
+# )
+# }
+# let(:stub_authority_request) {
+# stub_alma_get_request(
+# url: "bibs/authorities/#{authority_record_id}",
+# query: {view: "full"},
+# output: authority_record
+# )
+# }
+# let(:stub_second_authority_request) {
+# stub_alma_get_request(
+# url: "bibs/authorities/#{second_authority_record_id}",
+# query: {view: "full"},
+# output: second_authority_record
+# )
+# }
+# subject do
+# described_class.new(JSON.parse(@data))
+# end
+# context "#ids" do
+# it "returns an array of ids" do
+# expect(subject.ids).to contain_exactly(authority_record_id)
+# end
+# end
+
+# context "#authority_records" do
+# it "returns an array of Authority objects" do
+# stub_authority_request
+# expect(subject.authority_records.first.remediated_term).to eq("Undocumented immigrants")
+# end
+# end
+
+# context "#to_h" do
+# it "returns a flattend hash of the array of authority objects" do
+## Add an extra member to json
+# d = JSON.parse(@data)
+# d["member"].push({"id" => "999", "description" => "string"})
+# @data = d.to_json
+# stub_authority_request
+# stub_second_authority_request
+
+# expect(subject.to_h).to eq({
+# "undocumented foreign nationals" => "Undocumented immigrants",
+# "illegal aliens" => "Undocumented immigrants",
+# "illegal aliens--legal status, laws, etc." => "Undocumented immigrants",
+# "aliens--legal status, laws, etc." => "Undocumented immigrants",
+# "aliens, illegal" => "Undocumented immigrants",
+# "illegal immigrants" => "Undocumented immigrants",
+# "undocumented noncitizens" => "Undocumented immigrants",
+# "stuff--things--trivia" => "Whatever--Doesn't Matter--Another one"
+# })
+# end
+# end
+# context ".for" do
+# it "returns a Set from the Alma Set id" do
+# stub_set_request
+# expect(described_class.for(set_id).ids.first).to eq(authority_record_id)
+# end
+# it "errors out if it can't talk to alma" do
+# stub_alma_get_request(
+# url: "conf/sets/#{set_id}/members",
+# query: {limit: 100, offset: 0},
+# no_return: true
+# ).to_timeout
+# expect { described_class.for(set_id) }.to raise_error(StandardError, /#{set_id}/)
+# end
+# end
+# end
+# describe Jobs::TranslationMapGenerator::SubjectHeadingRemediation::ToRemediated::Authority do
+# before(:each) do
+# @data = JSON.parse(fixture("remediated_authority_record.json"))
+# end
+# subject do
+# described_class.new(@data)
+# end
+# let(:authority_record_id) { "12345" }
+# context ".for" do
+# it "errors out if it can't talk to Alma" do
+# stub_alma_get_request(
+# url: "bibs/authorities/#{authority_record_id}",
+# query: {view: "full"},
+# status: 500
+# )
+# expect { described_class.for(authority_record_id) }.to raise_error(StandardError, /#{authority_record_id}/)
+# end
+# end
+# context "#remeidated_term" do
+# it "returns the remediated term" do
+# expect(subject.remediated_term).to eq("Undocumented immigrants")
+# end
+# end
+# context "#deprecated_terms" do
+# it "returns the deprecated terms from the 450 field" do
+# expect(subject.deprecated_terms).to contain_exactly(
+# "Undocumented foreign nationals",
+# "Illegal aliens",
+# "Aliens--Legal status, laws, etc.",
+# "Aliens, Illegal",
+# "Illegal immigrants",
+# "Undocumented noncitizens"
+# )
+# end
+# end
+# context "#to_h" do
+# it "returns the expected deprecated_to_remediated hash with downcased terms" do
+# expect(subject.to_h).to eq({
+# "undocumented foreign nationals" => "Undocumented immigrants",
+# "illegal aliens" => "Undocumented immigrants",
+# "aliens, legal status, laws, etc." => "Undocumented immigrants",
+# "illegal aliens--legal status, laws, etc." => "Undocumented immigrants",
+# "aliens, illegal" => "Undocumented immigrants",
+# "illegal immigrants" => "Undocumented immigrants",
+# "undocumented noncitizens" => "Undocumented immigrants"
+# })
+# end
+
+# it "returns the hash with keys in reverse alphabetical order" do
+# expect(subject.to_h.keys).to eq([
+# "undocumented noncitizens",
+# "undocumented foreign nationals",
+# "illegal immigrants",
+# "illegal aliens--legal status, laws, etc.",
+# "illegal aliens",
+# "aliens, legal status, laws, etc.",
+# "aliens, illegal"
+# ])
+# end
+# end
+# end
