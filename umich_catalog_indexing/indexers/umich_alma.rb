d6d8137d46080bedd0b5655d6fa71f7398ed0db7
Monique Rio <mrio@umich.edu>
2024-08-13 16:24:37 -0400

    new_availablity is a flat array

--- umich_catalog_indexing/indexers/umich_alma.rb
+++ umich_catalog_indexing/indexers/umich_alma.rb
@@ -147,7 +147,7 @@ to_field "availability" do |record, acc, context|
 end
 
 to_field "new_availability" do |record, acc, context|
-  acc << Traject::UMich::Availability.new(context.clipboard[:ht][:hol_list]).to_a
+  acc.replace Traject::UMich::Availability.new(context.clipboard[:ht][:hol_list]).to_a
 end
 
 location_map = Traject::UMich.location_map
