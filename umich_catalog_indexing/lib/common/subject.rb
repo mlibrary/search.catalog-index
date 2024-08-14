5f345b948e87e9ab064882a25b89691ec1cbc8ef
Monique Rio <mrio@umich.edu>
2024-08-14 11:32:01 -0400

    tidy: linting

--- umich_catalog_indexing/lib/common/subject.rb
+++ umich_catalog_indexing/lib/common/subject.rb
@@ -10,10 +10,9 @@ require_relative "subject/non_lc_subject"
 require_relative "subject/normalize"
 
 module Common::Subject
-
   # We define subjects as being in any of these fields:
-  SUBJECT_FIELDS = %q(600 610 611 630 648 650 651 653 654 655 656 657 658 662 690)
-  
+  SUBJECT_FIELDS = "600 610 611 630 648 650 651 653 654 655 656 657 658 662 690"
+
   def self.subject_field?(field)
     SUBJECT_FIELDS.include?(field.tag)
   end
@@ -29,9 +28,9 @@ module Common::Subject
   # @param [MARC::DataField] field The field you want to try to match
   # @return [Array<MARC::DataField>] A (possibly empty) array of linked fields
   def self.linked_fields_for(record, field)
-    linking_id = field['6']
+    linking_id = field["6"]
     if linking_id
-      record.fields('880').select { |eef| eef['6']&.start_with? "#{field.tag}-#{linking_id.split("-").last}" }
+      record.fields("880").select { |eef| eef["6"]&.start_with? "#{field.tag}-#{linking_id.split("-").last}" }
     else
       []
     end
@@ -64,4 +63,3 @@ module Common::Subject
     end
   end
 end
-
