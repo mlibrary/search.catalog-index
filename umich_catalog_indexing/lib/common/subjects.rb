18918d09fa5766bac6dc1f24b3dc400496c74976
Monique Rio <mrio@umich.edu>
2024-08-14 11:57:23 -0400

    changes Common::Subject from module to class

--- umich_catalog_indexing/lib/common/subjects.rb
+++ umich_catalog_indexing/lib/common/subjects.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Common
-  module Subject
+  class Subject
   end
 end
 
@@ -9,57 +9,59 @@ require_relative "subjects/lc_subject"
 require_relative "subjects/non_lc_subject"
 require_relative "subjects/normalize"
 
-module Common::Subject
-  # We define subjects as being in any of these fields:
-  SUBJECT_FIELDS = "600 610 611 630 648 650 651 653 654 655 656 657 658 662 690"
+module Common
+  class Subject
+    # We define subjects as being in any of these fields:
+    SUBJECT_FIELDS = "600 610 611 630 648 650 651 653 654 655 656 657 658 662 690"
 
-  def self.subject_field?(field)
-    SUBJECT_FIELDS.include?(field.tag)
-  end
-
-  # Delegate LC determination to the class itself.
-  def self.lc_subject_field?(field)
-    LCSubject.lc_subject_field?(field)
-  end
-
-  # Determine the 880 (linking fields) for the given field. Should probably be pulled
-  # out into a more generically-available macro
-  # @param [MARC::Record] record The record
-  # @param [MARC::DataField] field The field you want to try to match
-  # @return [Array<MARC::DataField>] A (possibly empty) array of linked fields
-  def self.linked_fields_for(record, field)
-    linking_id = field["6"]
-    if linking_id
-      record.fields("880").select { |eef| eef["6"]&.start_with? "#{field.tag}-#{linking_id.split("-").last}" }
-    else
-      []
+    def self.subject_field?(field)
+      SUBJECT_FIELDS.include?(field.tag)
     end
-  end
 
-  # Get all the subject fields including associated 880 linked fields
-  # @param [MARC::Record] record The record
-  # @return [Array<MARC::DataField>] A (possibly empty) array of subject fields and their
-  # linked counterparts, if any
-  def self.subject_fields(record)
-    sfields = record.select { |field| subject_field?(field) }
-    sfields + sfields.flat_map { |field| linked_fields_for(record, field) }.compact
-  end
+    # Delegate LC determination to the class itself.
+    def self.lc_subject_field?(field)
+      LCSubject.lc_subject_field?(field)
+    end
 
-  # Get only the LC subject fields and any associated 880 linked fields
-  # @param [MARC::Record] record The record
-  # @return [Array<MARC::DataField>] A (possibly empty) array of LC subject fields and their
-  # linked counterparts, if any
-  def self.lc_subject_fields(record)
-    sfields = record.select { |field| lc_subject_field?(field) }
-    sfields + sfields.flat_map { |field| linked_fields_for(record, field) }.compact
-  end
+    # Determine the 880 (linking fields) for the given field. Should probably be pulled
+    # out into a more generically-available macro
+    # @param [MARC::Record] record The record
+    # @param [MARC::DataField] field The field you want to try to match
+    # @return [Array<MARC::DataField>] A (possibly empty) array of linked fields
+    def self.linked_fields_for(record, field)
+      linking_id = field["6"]
+      if linking_id
+        record.fields("880").select { |eef| eef["6"]&.start_with? "#{field.tag}-#{linking_id.split("-").last}" }
+      else
+        []
+      end
+    end
 
-  # Pass off a new subject to the appropriate class
-  def self.new(field)
-    if lc_subject_field?(field)
-      LCSubject.from_field(field)
-    else
-      NonLCSubject.new(field)
+    # Get all the subject fields including associated 880 linked fields
+    # @param [MARC::Record] record The record
+    # @return [Array<MARC::DataField>] A (possibly empty) array of subject fields and their
+    # linked counterparts, if any
+    def self.subject_fields(record)
+      sfields = record.select { |field| subject_field?(field) }
+      sfields + sfields.flat_map { |field| linked_fields_for(record, field) }.compact
+    end
+
+    # Get only the LC subject fields and any associated 880 linked fields
+    # @param [MARC::Record] record The record
+    # @return [Array<MARC::DataField>] A (possibly empty) array of LC subject fields and their
+    # linked counterparts, if any
+    def self.lc_subject_fields(record)
+      sfields = record.select { |field| lc_subject_field?(field) }
+      sfields + sfields.flat_map { |field| linked_fields_for(record, field) }.compact
+    end
+
+    # Pass off a new subject to the appropriate class
+    def self.new(field)
+      if lc_subject_field?(field)
+        LCSubject.from_field(field)
+      else
+        NonLCSubject.new(field)
+      end
     end
   end
 end
--- umich_catalog_indexing/lib/common/subjects/non_lc_subject.rb
+++ umich_catalog_indexing/lib/common/subjects/non_lc_subject.rb
@@ -2,16 +2,16 @@
 
 require_relative "lc_subject"
 
-module Common::Subject
-  # According to the LC docs, non-LC subjects follow the same formatting and data
-  # standards as LC, so this needs not do anything special until we learn otherwise.
-  class NonLCSubject < Common::Subject::LCSubject
-
-    # Here for testing purposes to distinguish from the LC subjects
-    # @return [Boolean]
-    def lc_subject_field? 
-      false
+module Common
+  class Subject
+    # According to the LC docs, non-LC subjects follow the same formatting and data
+    # standards as LC, so this needs not do anything special until we learn otherwise.
+    class NonLCSubject < Common::Subject::LCSubject
+      # Here for testing purposes to distinguish from the LC subjects
+      # @return [Boolean]
+      def lc_subject_field?
+        false
+      end
     end
-
   end
 end
