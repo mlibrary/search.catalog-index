c359a9b2d74d02ba8ffa1b8dc1003281052e84dd
Monique Rio <mrio@umich.edu>
2024-10-04 12:08:37 -0400

    adds some env vars to services

--- umich_catalog_indexing/lib/services/solr.rb
+++ umich_catalog_indexing/lib/services/solr.rb
@@ -1,7 +1,7 @@
-S.register(:solrcloud_on?) do
-  (ENV["SOLRCLOUD_ON"] == "true") ? true : false
-end
+S.register(:solrcloud_on?) { ENV["SOLRCLOUD_ON"] == "true" }
 S.register(:solr_threads) { ENV.fetch("SOLR_THREADS", 1).to_i }
 S.register(:solr_user) { ENV.fetch("SOLR_USER", "solr") }
 S.register(:solr_password) { ENV.fetch("SOLR_PASSWORD", "SolrRocks") }
 S.register(:processing_threads) { ENV.fetch("PROCESSING_THREADS", 1) }
+S.register(:supervisor_on?) { ENV["SUPERVISOR_ON"] == "true" }
+S.register(:sidekiq_supervisor_host) { ENV.fetch("SIDEKIQ_SUPERVISOR_HOST", "http://supervisor:3000") }
