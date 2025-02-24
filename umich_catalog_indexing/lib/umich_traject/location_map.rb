819446eccb919c274dabe76187cc8ee3db150d0d
Monique Rio <mrio@umich.edu>
2025-02-24 13:55:56 -0500

    remove outdated SPEC locations; add a couple catch all categories

--- umich_catalog_indexing/lib/umich_traject/location_map.rb
+++ umich_catalog_indexing/lib/umich_traject/location_map.rb
@@ -5,15 +5,14 @@ module Traject
       m = MatchMap.new
       m.echo = :onmiss
 
-      #====added 03-2023 for SCRC
+      m[/^AAEL ST[NOF]{1,2}/] = "AAEL CLOSD"
+
       m[/^SPEC SC[HB]-RARE.*/] = "SPEC RARE"
       m[/^SPEC SCB-SCI/] = "SPEC RARE"
       m[/^SPEC SCH-BUHR/] = "SPEC RARE"
       m[/^SPEC SCH-HAFTER/] = "SPEC RARE"
       m[/^SPEC SCB-BIND/] = "SPEC BIND"
       m[/^SPEC SC[HB]-REF/] = "SPEC REF"
-      #====end of 03-2023 SCRC additions
-      #====added 09-2022 for SCRC location addtions
       m[/^SPEC SCB-CHIL.*/] = "SPEC CHIL"
       m[/^SPEC SCH-FAUL.*/] = "SPEC FAUL"
       m[/^SPEC SC[HB]-TAUB.*/] = "SPEC TAUB"
@@ -22,19 +21,6 @@ module Traject
       m[/^SPEC SC[HB]-LA.*/] = "SPEC LABD" # need to fix this later
       m[/^SPEC SC[HB]-WOR/] = "SPEC WOR" # need to fix this later
       m[/^SPEC SCB-THC.*/] = "SPEC THC" # need to fix this later
-      #==== end of 09-2022 SCRC additions
-
-      m[/^SPEC RCLC/] = "SPEC CHIL"
-      m[/^SPEC GOSL/] = ["SPEC CHIL", "SPEC GOSL"]
-      m[/^SPEC CHIL.*/] = "SPEC CHIL"
-      m[/^SPEC CUL.*/] = "SPEC CUL"
-      m[/^SPEC WALP.*/] = "SPEC CHIL"
-      m[/^SPEC FAUL.*/] = "SPEC FAUL"
-      m[/^SPEC$/] = "SPEC"
-      m[/^SPEC TAUB.*/] = "SPEC TAUB"
-      m[/^SPEC LA.*/] = "SPEC LABD"
-      #=== between 09-2022 changes and here should be removed
-      #=== when confirmed that everything in SPEC is cleaned up
 
       m[/^HATCH AOFFNC/] = "HATCH ASIA"
       m[/^HATCH AOKA/] = "HATCH ASIA"
@@ -43,6 +29,10 @@ module Traject
       m[/^HATCH AOFF/] = "HATCH ASIA"
       m[/^HATCH AMIC/] = "HATCH ASIA"
       m[/^HATCH ASPEC/] = "HATCH ASIA"
+
+      m[/^HATCH CLARK/] = "HATCH CLARKBKS"
+      m[/^HATCH CLBKS/] = "HATCH CLARKBKS"
+      m[/^HATCH CLBKS-CIRC/] = "HATCH CLARKBKS"
 
       m[/^HATCH GRNT/] = "HATCH REF"
       m[/^HATCH GLRF/] = "HATCH REF"
