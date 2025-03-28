006b8324c75033f52f432862cc3f6444549b9e88
Monique Rio <mrio@umich.edu>
2025-03-28 15:20:35 -0400

    api record handles title

--- api/catalog_api/record.py
+++ api/catalog_api/record.py
@@ -1,5 +1,6 @@
 from catalog_api.solr_client import SolrClient
 
+
 def record_for(id: str):
     data = SolrClient().get_record(id)
     return Record(data)
@@ -8,11 +9,20 @@ def record_for(id: str):
 class Record:
     def __init__(self, data: dict):
         self.data = data
+        self.script = ["default", "vernacular"]
 
     @property
     def id(self):
-      return self.data["id"]
+        return self.data["id"]
 
     @property
     def title(self):
-       return self.data["title_display"]
+        return self._get_solr_paired_field("title_display")
+
+    def _get_solr_paired_field(self, key):
+        a = self.data.get(key)
+        if a:
+            return [
+                {"text": element, "script": self.script[index]}
+                for index, element in enumerate(a)
+            ]
