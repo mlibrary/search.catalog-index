cc8d823765556fb55700425707f5446bb3ef353e
Monique Rio <mrio@umich.edu>
2025-07-25 14:29:53 -0400

    WIP fastapi metrics

--- api/catalog_api/solr_client.py
+++ api/catalog_api/solr_client.py
@@ -1,6 +1,7 @@
 import requests
 from requests.auth import HTTPBasicAuth
 from catalog_api.services import S
+from prometheus_client import Histogram
 
 
 class NotFoundError(Exception):
@@ -8,12 +9,17 @@ class NotFoundError(Exception):
 
 
 class SolrClient:
+    SOLR_HISTOGRAM = Histogram(
+        "solr_catalog_record_request_duration_seconds", "Length of solr requests"
+    )
+
     def __init__(self) -> None:
         self.session = requests.Session()
         self.base_url = f"{S.solr_url}/solr/biblio"
         if S.solr_cloud_on:
             self.session.auth = HTTPBasicAuth(S.solr_user, S.solr_password)
 
+    @SOLR_HISTOGRAM.time()
     def get_record(self, id: str):
         params = {"q": f"id:{id}"}
         url = f"{self.base_url}/select"
